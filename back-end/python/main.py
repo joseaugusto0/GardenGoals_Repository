746e198e5a89d03afb1a96a280fddffad34f338a
José Augusto Coura de Oliveira <jaugustocoura@gmail.com>
2022-08-20 00:02:21 -0300

    feat: added a class to process geometry functions
    Actually has just the method to bring polygon most close to the origin as possible

--- back-end/python/main.py
+++ back-end/python/main.py
@@ -1,4 +1,5 @@
 import json
+from solvers.optimizers.geometry_functions import GeometryFunctions
 from entities.Coordenates import PolygonBuilder
 with open("min_input.json", "r") as input:
     min_input: Dict = json.load(input)
@@ -7,3 +8,8 @@ with open("min_input.json", "r") as input:
 polygonBuilder = PolygonBuilder()
 polygonBuilder._set_infos_from_json(min_input)
 polygon = polygonBuilder._get_polygon()
+
+geometryClass = GeometryFunctions()
+geometryClass._set_input(polygon)
+geometryClass._get_polygon_to_zero()
+polygon = geometryClass._get_output()
---
+++ back-end/python/solvers/optimizers/geometry_functions.py
@@ -0,0 +1,53 @@
+from typing import Dict
+from entities.Polygon import Polygon
+from matplotlib import pyplot as plt
+
+
+class GeometryFunctions:
+
+    def __init__(self):
+
+        self.__input: Polygon = None
+        self._output: Polygon = None
+        self._polygon_coordenates_in_zero = []
+
+    def _find_point_closest_to_zero(self) -> Dict[float, float]:
+        minor_x_value = None
+        minor_y_value = None
+        closest_to_zero = None
+        cont = 0
+
+        for j in self.__input.coordenates:
+            if cont==0:
+                minor_x_value = abs(j['lat'])
+                minor_y_value = abs(j['lng'])
+                cont += 1
+                
+            if abs(j['lat'])<=minor_x_value and abs(j['lng'])<=minor_y_value:
+                minor_x_value = j['lat']
+                minor_y_value = j['lng']
+                closest_to_zero = {"lat":j['lat'], "lng":j['lng']}
+
+        return closest_to_zero
+
+    def _get_polygon_to_zero(self):
+        
+        closest_to_zero = self._find_point_closest_to_zero()
+        x_difference = abs(closest_to_zero['lat'])
+        y_difference = abs(closest_to_zero['lng'])
+
+        for coordenate in self.__input.coordenates:
+            self._polygon_coordenates_in_zero.append({"lat": coordenate['lat']-x_difference,"lng": coordenate['lng']-y_difference})
+
+        self._show_polygon(self._polygon_coordenates_in_zero)
+        self._output.polygonToZero=self._polygon_coordenates_in_zero
+        
+    def _show_polygon(self, coordenates: Dict):
+        plt.plot([x['lat'] for x in coordenates], [y['lng'] for y in coordenates], marker = "o")
+        plt.show()
+
+    def _set_input(self, input: Dict):
+        self.__input = input
+
+    def _get_output(self) -> Dict:
+        return self._output
