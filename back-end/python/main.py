8a579b942073e06fd063dd95537a6add72ac9d92
José Augusto Coura de Oliveira <jaugustocoura@gmail.com>
2022-08-20 15:33:24 -0300

    feat: added basic optimization for rectangle vegetable garden

--- back-end/python/main.py
+++ back-end/python/main.py
@@ -1,5 +1,7 @@
 import json
+from typing import Dict
 from solvers.optimizers.geometry_functions import GeometryFunctions
+from solvers.optimizers.otimization_vegetable_garden import CalculateRectangleVegetableGarden
 from entities.Polygon import PolygonBuilder
 
 with open("min_input.json", "r") as input:
@@ -14,3 +16,11 @@ geometryClass = GeometryFunctions()
 geometryClass._set_input(polygon)
 geometryClass._solve_rectangle()
 polygon = geometryClass._get_output()
+
+if polygon.polygonType=="rectangle":
+    rectangleOptimizer = CalculateRectangleVegetableGarden()
+    rectangleOptimizer._set_input(polygon)
+    rectangleOptimizer.solve()
+    print(rectangleOptimizer.get_output())
+
+
--- back-end/python/solvers/optimizers/otimization_vegetable_garden.py
+++ back-end/python/solvers/optimizers/otimization_vegetable_garden.py
@@ -3,25 +3,25 @@ import os
 from typing import Dict
 from ortools.linear_solver import pywraplp
 from ortools.linear_solver.pywraplp import Variable
+from entities.Polygon import Polygon
+from entities.RectangleOptimized import RectangleOptimizedBuilder
 
-class CalculateVegetableGarden:
+class CalculateRectangleVegetableGarden:
 
-    def __init__(self, food: str = 'lettuce', width: int = 100, lenght: int = 200):
+    def __init__(self, food: str = 'lettuce'):
+        self.__input: Polygon = None
+        self._output:Polygon = None
+        
         self.number_of_lines: Variable = None
         self.number_of_plants: Variable = None
-        print(os.listdir())
-        self.measurements = json.load(open('sizes.json'))
-        self.food_choosen_infos = self.select_food_infos(food)
-        self.solver = None
-        self.width = width #cm
-        self.lenght = lenght #cm
-        self.output = {
-            "Lines": 0,
-            "Plants": 0
-        }
 
+        self.food_choosen_infos = self.select_food_infos(food)
+        self.values_solved = {"lines": 0, "plants": 0}
+        self.solver = None
+        
     def select_food_infos(self, food: str) -> Dict:
-        return self.measurements[food]
+        measurements = json.load(open('./solvers/optimizers/sizes.json'))
+        return measurements[food]
 
     def config_solver(self, solver_type: str = "GLOP"):
         self.solver = pywraplp.Solver.CreateSolver(solver_type)   
@@ -35,13 +35,13 @@ class CalculateVegetableGarden:
         #Defining rule to number of lines
         reference = self.food_choosen_infos["width max"]*.85 if self.food_choosen_infos["width max"]>self.food_choosen_infos["space between lines"] else self.food_choosen_infos["space between lines"]
         self.solver.Add(
-            self.number_of_lines * reference <= self.width
+            self.number_of_lines * reference <= self.__input.width
         )
 
         #Defining rule to number of plants
         reference = self.food_choosen_infos["width max"]*.85 if self.food_choosen_infos["width max"]>self.food_choosen_infos["space between plants"] else self.food_choosen_infos["space between plants"]
         self.solver.Add(
-            self.number_of_plants * reference <= self.lenght
+            self.number_of_plants * reference <= self.__input.height
         )
 
     def define_lines_objetive(self):
@@ -58,9 +58,9 @@ class CalculateVegetableGarden:
 
         self.define_lines_objetive()
         results_lines = self.solver.Solve()
-
+        
         if results_lines == pywraplp.Solver.OPTIMAL:
-            self.output['Lines'] = self.number_of_lines.solution_value()
+            self.values_solved['lines'] = self.number_of_lines.solution_value()
         else:
             print('The problem does not have an optimal solution.')
             raise     
@@ -69,14 +69,26 @@ class CalculateVegetableGarden:
         results_plants = self.solver.Solve()
 
         if results_plants == pywraplp.Solver.OPTIMAL:
-            self.output['Plants'] = self.number_of_plants.solution_value()
+            self.values_solved['plants'] = self.number_of_plants.solution_value()
         else:
             print('The problem does not have an optimal solution.')
             raise
-    
-    def get_output(self):
-        return self.output
 
-optimizer = CalculateVegetableGarden()
-optimizer.solve()
-print(optimizer.get_output())
+        
+        rectangle_optimized_builder = RectangleOptimizedBuilder()
+        rectangle_optimized_builder._set_infos_from_json(self.values_solved)
+        self.__input.rectangle_optimized = rectangle_optimized_builder._get_rectangle_optimized()
+        self._output = self.__input
+
+    
+    def _set_input(self, input: Polygon):
+        self.__input = input
+
+    def get_output(self) -> Dict:
+        return self._output
+
+
+if __name__=="__main__":
+    optimizer = CalculateRectangleVegetableGarden()
+    optimizer.solve()
+    print(optimizer.get_output())
