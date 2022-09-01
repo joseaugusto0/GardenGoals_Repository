8af78bdaeb980e96f9c83612814e88019e162cd2
José Augusto Coura de Oliveira <jaugustocoura@gmail.com>
2022-09-01 19:11:39 -0300

    feat: creating service to create a new plant info

---
+++ back-end/api/src/services/CreatePlantInfosService.ts
@@ -0,0 +1,37 @@
+import { AppDataSource } from "../../data_source"
+import { PlantInfos } from "../entities/PlantInfos"
+
+interface IPlantInfosRequest {
+    name: string
+    space_between_lines: number,
+    space_between_plants: number,
+}
+
+class CreatePlantInfoService {
+
+    async execute({name, space_between_lines, space_between_plants}:IPlantInfosRequest) {
+
+        if ((!name) || (!space_between_lines) || (!space_between_plants)){
+            throw new Error("Fill all infos")
+        }
+
+        const plantInfosRepository = AppDataSource.getRepository(PlantInfos)
+        const plantInfoExists = await plantInfosRepository.findOneBy({
+            name
+        })
+
+        if(plantInfoExists){
+            throw new Error("Plant already exists")
+        }
+
+        const new_plant = plantInfosRepository.create({
+            name, 
+            space_between_lines,
+            space_between_plants
+        })
+
+        plantInfosRepository.save(new_plant)
+
+        return new_plant
+    }
+}
