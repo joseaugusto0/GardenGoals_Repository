e06a5a1d7880d1468d98cfd9256fdde9e5fbd136
José Augusto Coura de Oliveira <jaugustocoura@gmail.com>
2022-09-04 22:09:32 -0300

    feat: added migration directly

--- back-end/api/src/data_source.ts
+++ back-end/api/src/data_source.ts
@@ -1,10 +1,12 @@
 import { DataSource } from "typeorm"
+import { CreatePlantInfos1662069261633 } from "./database/migrations/1662069261633-CreatePlantInfos"
 import { PlantInfos } from "./entities/PlantInfos"
 
 
 const AppDataSource = new DataSource({
     type: "postgres",
     url: process.env["DATABASE_URL"],
+    migrations: [CreatePlantInfos1662069261633],
     port: parseInt(process.env["PGPORT"]),
     username: process.env["PGUSER"],
     password: process.env["PGPASSWORD"],
