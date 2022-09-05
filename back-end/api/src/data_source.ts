0e017999203a272443b2006a0d1b963f1a3f2026
José Augusto Coura de Oliveira <jaugustocoura@gmail.com>
2022-09-04 22:01:32 -0300

    fix: trying to fix DataSource

--- back-end/api/src/data_source.ts
+++ back-end/api/src/data_source.ts
@@ -1,10 +1,15 @@
 import { DataSource } from "typeorm"
+import { PlantInfos } from "./entities/PlantInfos"
 
 
 const AppDataSource = new DataSource({
     type: "postgres",
     url: process.env["DATABASE_URL"],
-    migrations: ['./src/database/migrations/*.ts'],
+    port: process.env["PGPORT"],
+    username: process.env["PGUSER"],
+    password: process.env["PGPASSWORD"],
+    database: process.env["PGDATABASE"],
+    entities: [PlantInfos]
 })
 
 AppDataSource.initialize()
