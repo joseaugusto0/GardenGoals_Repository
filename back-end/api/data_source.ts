e4dae065d6a2d342267ecc67a823f31a0f73764a
José Augusto Coura de Oliveira <jaugustocoura@gmail.com>
2022-09-04 21:02:10 -0300

    feat: configuring to deploy back-end in Railway

--- back-end/api/data_source.ts
+++ back-end/api/data_source.ts
@@ -1,8 +1,9 @@
 import { DataSource } from "typeorm"
 
+
 const AppDataSource = new DataSource({
-    type: "sqlite",
-    database: "./src/database/users.sqlite",
+    type: "postgres",
+    url: process.env["DATABASE_URL"],
     entities: ['./src/entities/*.ts'],
     migrations: ['./src/database/migrations/*.ts'],
 })
