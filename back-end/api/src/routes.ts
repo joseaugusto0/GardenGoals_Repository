20f21dad6eebc932719d1600946335c5f3aa0124
José Augusto Coura de Oliveira <jaugustocoura@gmail.com>
2022-09-04 21:49:28 -0300

    feat: added a get route to test

--- back-end/api/src/routes.ts
+++ back-end/api/src/routes.ts
@@ -1,4 +1,4 @@
-import { Router } from "express"
+import { Router, Request, Response } from "express"
 import { CreatePlantInfosController } from "./controller/CreatePlantsInfosController";
 import { GetAllPlantInfosController } from "./controller/GetAllPlantInfosController";
 import { RunSolverController } from "./controller/RunSolverController";
@@ -9,6 +9,9 @@ const createPlantInfoController = new CreatePlantInfosController();
 const runSolverController = new RunSolverController();
 const getAllPlantInfosController =  new GetAllPlantInfosController();
 
+router.get("/", (request: Request, response: Response) => {
+    response.status(200).send("Foi")
+})
 router.post("/plant_infos", createPlantInfoController.handle)
 router.post("/run_solver", runSolverController.handle)
 router.post("/get_plant_infos", getAllPlantInfosController.handle)
