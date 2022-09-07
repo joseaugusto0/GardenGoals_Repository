310ed3ab2243622c52283d33d30fb72fefbb1ecd
José Augusto Coura de Oliveira <jaugustocoura@gmail.com>
2022-09-07 16:25:55 -0300

    feat: added two different databases, selecting depending if is development or not

--- back-end/api/src/data_source.ts
+++ back-end/api/src/data_source.ts
@@ -1,17 +1,28 @@
 import { DataSource } from "typeorm"
 
+var data = null
+if (process.env.DEV){
+    data = {
+        type : "sqlite",
+        database: './src/database/db.sqlite',
+        entities: ["./src/entities/*.ts"],
+        migrations: ["./src/database/migrations/*.ts"],
+    }
+}else{
+    data = {
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        
+        port: parseInt(process.env.PGPORT),
+        username: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        entities: ["dist/entities/*.js"],
+        migrations: ["dist/database/migrations/*.js"],
+    }
+}
 
-const AppDataSource = new DataSource({
-    type: "postgres",
-    url: process.env.DATABASE_URL,
-    
-    port: parseInt(process.env.PGPORT),
-    username: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    entities: ["dist/entities/*.js"],
-    migrations: ["dist/database/migrations/*.js"],
-})
+const AppDataSource = new DataSource(data)
 
 AppDataSource.initialize()
     .then(() => {
